84493e096820601d1dbf01b79bdd224253e146a3
davidblumenthal <github@blumenthal.com>
2014-02-10 15:44:07 -0600

    Load header stuff locally then refresh

--- www/js/fx_phonegap.js
+++ www/js/fx_phonegap.js
@@ -1,12 +1,29 @@
-// Wait for PhoneGap to load
-// 
+//The rest of the data on the web site
+var WEB_URL = "https://gateway.fundsxpress.com/files/fxAir2013/draft1/login.html";
+
+
+//Wait for PhoneGap to load
+//
 document.addEventListener("deviceready", onDeviceReady, false);
 
 // PhoneGap is loaded and it is now safe to make calls PhoneGap methods
 //
 function onDeviceReady() {
+	//http://demos.jquerymobile.com/1.2.0/docs/pages/phonegap.html
+	//Try to force jQuery to think that the browser supports 
+	//Cross Origin Resource Sharing
+	$.support.cors = true;
+	
+	//Not allowed to load a remote page unless this is true
+	$.mobile.allowCrossDomainPages = true;
+	
     checkConnection();
-}
+    console.log("here1");
+    $( ":mobile-pagecontainer" ).pagecontainer( "change", WEB_URL );
+    //window.location = "https://gateway.fundsxpress.com/files/fxAir2013/draft1/login.html";
+    console.log("here2");
+}//onDeviceReady
+
 
 function checkConnection() {
     var networkState = navigator.network.connection.type;
